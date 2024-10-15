d8232565c7e08abafb15ab7973da4acd7e543694
alejaandrofdez <afp0042@alu.medac.es>
2024-10-15 15:20:26 +0200

    cambios

---
+++ src/Ejer1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ejer1 {
+    public static void main(String[] args) throws Exception {
+
+        for (int i=0; i < 100; i++)
+            System.out.println(i);
+
+    }
+}
