f1fbcdeee80f830df5e59efa64a02b47539ea5f5
alejaandrofdez <afp0042@alu.medac.es>
2024-10-15 20:40:38 +0200

    repaso java

---
+++ src/Repaso3.java
@@ -0,0 +1,18 @@
+public class Repaso3 {
+    public static void main(String[] args) throws Exception{
+ 
+
+/*ordenar con array y funcion */
+
+        int[] entrada= {3,4,5,2,1};
+        int[] ord=ordenarAsc(entrada);
+        for(int i=0; i<ord.length; i++){
+            System.out.println(ord[i]);
+        }
+    } 
+    public static int[] ordenarAsc ( int[] numeros) {
+        int[] ordenados = {1,2,3,4,5};
+
+        return ordenados;
+    }
+}
